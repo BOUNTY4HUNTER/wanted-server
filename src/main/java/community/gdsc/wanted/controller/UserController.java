c68dceb1bd672c818084e698249b094e5fe42074
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-10-22 20:12:17 +0900

    #User - UserController 작성
    user에 대한 모든 API 작성 예정

---
+++ src/main/java/community/gdsc/wanted/controller/UserController.java
@@ -0,0 +1,17 @@
+package community.gdsc.wanted.controller;
+
+import community.gdsc.wanted.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+}
