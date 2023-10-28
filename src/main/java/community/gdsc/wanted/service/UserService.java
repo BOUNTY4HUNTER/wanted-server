2a40cd135c7d44daf8818f948c42788d160f12d1
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-10-28 19:35:20 +0900

    Refactor: UserService에서 메일 관련 코드 삭제

--- src/main/java/community/gdsc/wanted/service/UserService.java
+++ src/main/java/community/gdsc/wanted/service/UserService.java
@@ -13,7 +13,6 @@ import community.gdsc.wanted.Base.BaseException;
 import community.gdsc.wanted.RequestModel.SignupRequest;
 import community.gdsc.wanted.domain.User;
 import community.gdsc.wanted.repository.UserRepository;
-import community.gdsc.wanted.utils.MailHandler;
 import lombok.RequiredArgsConstructor;
 
 @Transactional
@@ -113,79 +112,5 @@ public class UserService {
 		userRepository.deleteById(id);
 
 		// TODO: 예외 throw
-	}
-
-	public void findUserId(String email) throws BaseException {
-		String name;
-		Integer userId;
-		try {
-			//해당 이메일을 가진 userEntity찾기
-			try {
-				User user = userRepository.findByEmail(email);
-				System.out.println("-------------해당이메일이 있느냐?------------" + user);
-				//userEntity에서 name과 userId 추출 후 해당이메일로 보내기
-				name = user.getUsername();
-				userId = user.getId();
-			} catch (Exception e) {
-				throw new BaseException(INVALID_EMAIL);
-			}
-
-			System.out.println("---------name & userId--------" + name + "     " + userId);
-
-			//이메일보내기
-			MailHandler mailHandler = new MailHandler(javaMailSender);
-			mailHandler.setFrom(from); //이거 naver메일 보낼때는 필수다!!!!!!
-			mailHandler.setTo(email);
-			mailHandler.setSubject("[Wanted] " + name + "님의 아이디를 보내드립니다.");
-			mailHandler.setText(name + "님의 아이디: " + userId);
-			mailHandler.send();
-
-			System.out.println("-------메일 발송이 되었느냐??--------");
-		} catch (Exception e) {
-			throw new BaseException(DATABASE_ERROR);
-		}
-	}
-
-	//랜덤함수로 임시비밀번호 구문 만들기
-	public String getTempPassword() {
-		char[] charSet = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-			'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-		String str = "";
-
-		// 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
-		int idx = 0;
-		for (int i = 0; i < 10; i++) {
-			idx = (int)(charSet.length * Math.random());
-			str += charSet[idx];
-		}
-		return str;
-	}
-
-	//비밀번호 찾기
-	public void findPassword(String username) throws BaseException {
-		try {
-			User user = userRepository.findByUsername(username);
-
-			String name = user.getUsername();
-
-			String tmpPassword = getTempPassword();
-
-			String email = user.getEmail();
-
-			user.setPassword(tmpPassword);
-
-			userRepository.save(user);
-
-			//해당 아이디가 존재하면 해당 이메일로 메일보내기
-			MailHandler mailHandler = new MailHandler(javaMailSender);
-			mailHandler.setTo(email);
-			mailHandler.setSubject("[Wanted] " + name + "님의 임시비밀번호 안내 이메일입니다.");
-			mailHandler.setText(name + "님, 안녕하세요. Wanted 임시비밀번호 안내 관련 이메일 입니다." + " 회원님의 임시 비밀번호는 "
-				+ tmpPassword + " 입니다." + "로그인 후에 비밀번호를 변경을 해주세요");
-			mailHandler.send();
-		} catch (Exception exception) {
-			throw new BaseException(INVALID_USER_ID);
-		}
 	}
 }
