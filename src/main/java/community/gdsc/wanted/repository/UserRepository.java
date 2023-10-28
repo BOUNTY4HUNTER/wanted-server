04fe0c32d09fc792b5c46cc47ecd574530467d81
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-10-28 16:56:57 +0900

    Feat: findByEmail 추가
    email로 해당 user를 찾는 findByEmail 추가

--- src/main/java/community/gdsc/wanted/repository/UserRepository.java
+++ src/main/java/community/gdsc/wanted/repository/UserRepository.java
@@ -1,19 +1,18 @@
 package community.gdsc.wanted.repository;
 
-import community.gdsc.wanted.domain.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+import community.gdsc.wanted.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findById(int id);
+	User findById(int id);
 
-    User findByUsername(String username);
+	User findByUsername(String username);
 
-    void deleteById(Integer id);
+	User findByEmail(String email);
+
+	void deleteById(Integer id);
 }
 
