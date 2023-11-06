b57725d6df82ef2a6ea47b9b0eb77ca9414ae0c7
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-11-06 16:43:33 +0900

    Test: 중복 테스트 메서드 삭제, /삭제

--- src/test/java/community/gdsc/wanted/user/UserEmailTest.java
+++ src/test/java/community/gdsc/wanted/user/UserEmailTest.java
@@ -49,129 +49,6 @@ public class UserEmailTest {
 	}
 
 	@Test
-	void signup() throws Exception {
-		SignupRequestDTO dto = new SignupRequestDTO(
-			"testtest",
-			"testtest",
-			"jongha",
-			"kim",
-			"12191579@inha.edu",
-			"asdf",
-			"인천광역시",
-			"서구",
-			"마전동"
-		);
-
-		String body = new ObjectMapper().writeValueAsString(
-			dto
-		);
-
-		mvc.perform(
-				post("/api/user")
-					.content(body)
-					.contentType(MediaType.APPLICATION_JSON)
-					.characterEncoding("utf-8")
-			)
-			.andDo(MockMvcResultHandlers.print())
-			.andExpect(status().isOk())
-			.andReturn();
-
-		Assertions.assertEquals(
-			userRepository.findByUsername("testtest"),
-			dto.toEntity()
-		);
-	}
-
-	@Test
-	void signupFailByIllegalArgs() throws Exception {
-		SignupRequestDTO dto = new SignupRequestDTO(
-			"testtest",
-			"tes",
-			"jongha",
-			"kim",
-			"12191579@inha.edu",
-			"asdf",
-			"인천광역시",
-			"서구",
-			"마전동"
-		);
-
-		String body = new ObjectMapper().writeValueAsString(
-			dto
-		);
-
-		mvc.perform(
-				post("/api/user")
-					.content(body)
-					.contentType(MediaType.APPLICATION_JSON)
-					.characterEncoding("utf-8")
-			)
-			.andDo(MockMvcResultHandlers.print())
-			.andExpect(status().isBadRequest())
-			.andReturn();
-	}
-
-	@Test
-	void getUserById() throws Exception {
-		int id = userRepository.save(
-			User.builder()
-				.username("testtest")
-				.password("testtest")
-				.firstName("kim")
-				.lastName("jongha")
-				.email("12191579@inha.edu")
-				.nickname("asdf")
-				.regionDepth1("인천광역시")
-				.regionDepth2("서구")
-				.regionDepth3("마전동")
-				.build()
-		).getId();
-
-		mvc.perform(get("/api/user/" + id))
-			.andDo(MockMvcResultHandlers.print())
-			.andExpect(status().isOk())
-			.andExpect(MockMvcResultMatchers.jsonPath("$.username").value("testtest"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("kim"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("jongha"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.email").value("12191579@inha.edu"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("asdf"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.regionDepth1").value("인천광역시"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.regionDepth2").value("서구"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.regionDepth3").value("마전동"))
-			.andReturn();
-	}
-
-	@Test
-	void login() throws Exception {
-		userRepository.save(
-			User.builder()
-				.username("testtest")
-				.password(passwordEncoder.encode("testtest"))
-				.firstName("kim")
-				.lastName("jongha")
-				.email("12191579@inha.edu")
-				.nickname("asdf")
-				.regionDepth1("인천광역시")
-				.regionDepth2("서구")
-				.regionDepth3("마전동")
-				.build()
-		);
-
-		String body = new ObjectMapper().writeValueAsString(
-			new SigninRequestDTO("testtest", "testtest")
-		);
-
-		mvc.perform(
-				post("/api/user/signin")
-					.content(body)
-					.characterEncoding("utf-8")
-					.contentType(MediaType.APPLICATION_JSON)
-			)
-			.andDo(MockMvcResultHandlers.print())
-			.andExpect(status().isOk());
-	}
-
-	@Test
 	void findId() throws Exception {
 		String email = userRepository.save(
 			User.builder()
@@ -188,7 +65,7 @@ public class UserEmailTest {
 		).getEmail();
 
 		mvc.perform(
-				get("/api/user/login/forgot/id/")
+				get("/api/user/login/forgot/id")
 					.queryParam("email",email)
 			)
 			.andDo(MockMvcResultHandlers.print())
@@ -213,7 +90,7 @@ public class UserEmailTest {
 		).getUsername();
 
 		mvc.perform(
-				get("/api/user/login/forgot/password/")
+				get("/api/user/login/forgot/password")
 					.queryParam("id",username)
 			)
 			.andDo(MockMvcResultHandlers.print())
