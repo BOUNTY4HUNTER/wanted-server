a7f6df34aa66b88419bf18f898ed2f1f67900e37
Godbell <12191579@inha.edu>
2023-11-15 03:06:17 +0900

    Revert "Feat: 찾은 물건 게시 API 생성"
    This reverts commit 4833eebd2c2c5c81ca0a9d7f4d271210997bc364.

--- src/main/java/community/gdsc/wanted/repository/FoundRepository.java
+++
@@ -1,8 +0,0 @@
-package community.gdsc.wanted.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import community.gdsc.wanted.domain.Found;
-
-public interface FoundRepository extends JpaRepository<Found, Integer> {
-}
---
+++ src/test/java/community/gdsc/wanted/WantedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package community.gdsc.wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
