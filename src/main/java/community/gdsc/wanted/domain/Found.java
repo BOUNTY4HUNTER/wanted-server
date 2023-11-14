4833eebd2c2c5c81ca0a9d7f4d271210997bc364
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2023-11-15 03:01:49 +0900

    Feat: 찾은 물건 게시 API 생성
    * Chore: Config Project
    - MySQL, Redis, MongoDB 연동
    - 파일 권한 조정
    
    * Fix: application.yaml MongoDB 유저명 키 수정
    
    * Fix: lombok dependency 종류 변경
    - implementation은 gradle 5 버전 미만까지 사용하던 방식
    - gradle 버전에 맞도록 compileOnly, annotationProcessor로 설정
    
    * 찾은 게시물에 대한 등록, 수정, 삭제
    
    * 찾은 게시물에 대한 등록, 수정, 삭제
    
    * 찾은 게시물에 대한 등록, 수정, 삭제
    
    * 찾은 게시물에 대한 등록, 수정, 삭제
    
    * 찾았습니다 게시물
    
    * 조회 및 리스트
    
    * 1차 수정본
    
    * 2차 수정본
    
    * 수정본
    
    ---------
    
    Co-authored-by: Godbell <12191579@inha.edu>

---
+++ src/main/java/community/gdsc/wanted/domain/Found.java
@@ -0,0 +1,64 @@
+package community.gdsc.wanted.domain;
+
+import java.sql.Timestamp;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import community.gdsc.wanted.DTO.FoundListResponseDto;
+import community.gdsc.wanted.DTO.FoundResponseDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "found")
+@Entity
+@Data
+public class Found {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "author_idx")
+    private Integer authorIdx;
+
+    @Column(name = "title", length = 100, nullable = false)
+    private String title;
+
+    @Column(name = "content", length = 5000, nullable = false)
+    private String content;
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Timestamp created_at;
+
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    private Integer is_deleted = 0;
+
+    public FoundResponseDto toViewResponse() {
+        return new FoundResponseDto(
+            id,
+            title,
+            content
+        );
+    }
+
+    public FoundListResponseDto toListResponse() {
+        return new FoundListResponseDto(
+            this.getId(),
+            this.getTitle(),
+            this.getCreated_at()
+        );
+    }
+}
